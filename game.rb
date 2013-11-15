18424da32133356580a2b30d05c86399a549715a
Kevin <kvnkwon@gmail.com>
2013-11-15 10:38:59 -0500

    made it harder

--- game.rb
+++ game.rb
@@ -1,9 +1,14 @@
 #created by Kevin Kwon
+#updated to make it harder.
 puts "CONFUCIUS SAY -> Guess a number between 0 and 100:"
-puts "You have 10 tries. GOOD LUCK!"
+puts "====Press ENTER to continue.===="
+gets
+puts "You have 5 tries. GOOD LUCK!"
+puts '====Press ENTER to continue.===='
+gets
+puts 'CONFUCIUS SAY: START!'
 win = rand(100).to_i
-puts win
-tries = 10
+tries = 5
 while tries > 0
   guess = gets.chomp
   if guess.to_i < 0 || guess.to_i > 100 || guess =~ /[A-Za-z]/
