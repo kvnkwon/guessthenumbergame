68f9b66f5da9ba3ef4c079ae313c15f7296a1d70
Kevin <kvnkwon@gmail.com>
2013-11-15 10:50:12 -0500

    made it more interactive

--- game.rb
+++ game.rb
@@ -1,34 +1,78 @@
 #created by Kevin Kwon
 #updated to make it harder.
-puts "CONFUCIUS SAY -> Guess a number between 0 and 100:"
-puts "====Press ENTER to continue.===="
-gets
-puts "You have 5 tries. GOOD LUCK!"
-puts '====Press ENTER to continue.===='
-gets
-puts 'CONFUCIUS SAY: START!'
-win = rand(100).to_i
-tries = 5
-while tries > 0
-  guess = gets.chomp
-  if guess.to_i < 0 || guess.to_i > 100 || guess =~ /[A-Za-z]/
-    puts "CONFUCIUS SAY YOU STUPID! FOLLOW THE RULES!"
-    puts "Guess a number between 0 and 100:"
-    tries = tries - 1
-    puts "You have #{tries} tries left."
-  elsif guess.to_i > win
-    puts "CONFUCIUS SAY GUESS TOO HIGH STUPID! TRY AGAIN:"
-    tries = tries - 1
-    puts "You have #{tries} tries left."
-  elsif guess.to_i < win
-    puts "CONFUCIUS SAY GUESS TOO LOW STUPID! TRY AGAIN:"
-   tries = tries - 1
-    puts "You have #{tries} tries left."
-  else
-    puts "CONFUCIUS SAY YOU WISE LIKE THE HERMIT!"
-    abort
+while true
+  puts "CONFUCIUS SAY -> Guess a number between 0 and 100:"
+  puts "====Press ENTER to continue.===="
+  gets
+  puts "You have 5 tries. GOOD LUCK!"
+  puts '====Press ENTER to continue.===='
+  gets
+  puts 'CONFUCIUS SAY: START!'
+  win = rand(100).to_i
+  tries = 5
+  while tries > 0
+    guess = gets.chomp
+    if guess.to_i < 0 || guess.to_i > 100 || guess =~ /[A-Za-z]/
+      puts "CONFUCIUS SAY YOU STUPID! FOLLOW THE RULES!"
+      puts "Guess a number between 0 and 100:"
+      tries = tries - 1
+      puts "You have #{tries} tries left."
+    elsif guess.to_i > win
+      puts "CONFUCIUS SAY GUESS TOO HIGH STUPID! TRY AGAIN:"
+      tries = tries - 1
+      puts "You have #{tries} tries left."
+    elsif guess.to_i < win
+      puts "CONFUCIUS SAY GUESS TOO LOW STUPID! TRY AGAIN:"
+     tries = tries - 1
+      puts "You have #{tries} tries left."
+    else
+      puts "CONFUCIUS SAY YOU WISE LIKE THE HERMIT!"
+      puts "░░░░░░░░░▄██████████▄▄░░░░░░░░
+░░░░░░▄█████████████████▄░░░░░
+░░░░░██▀▀▀▀▀▀▀▀▀▀▀████████░░░░
+░░░░██░░░░░░░░░░░░░░███████░░░
+░░░██░░░░░░░░░░░░░░░████████░░
+░░░█▀░░░░░░░░░░░░░░░▀███████░░
+░░░█▄▄██▄░░░▀█████▄░░▀██████░░
+░░░█▀███▄▀░░░▄██▄▄█▀░░░█████▄░
+░░░█░░▀▀█░░░░░▀▀░░░▀░░░██░░▀▄█
+░░░█░░░█░░░▄░░░░░░░░░░░░░██░██
+░░░█░░█▄▄▄▄█▄▀▄░░░░░░░░░▄▄█▄█░
+░░░█░░█▄▄▄▄▄▄░▀▄░░░░░░░░▄░▀█░░
+░░░█░█▄████▀██▄▀░░░░░░░█░▀▀░░░
+░░░░██▀░▄▄▄▄░░░▄▀░░░░▄▀█░░░░░░
+░░░░░█▄▀░░░░▀█▀█▀░▄▄▀░▄▀░░░░░░
+░░░░░▀▄░░░░░░░░▄▄▀░░░░█░░░░░░░
+░░░░░▄██▀▀▀▀▀▀▀░░░░░░░█▄░░░░░░
+░░▄▄▀░░░▀▄░░░░░░░░░░▄▀░▀▀▄░░░░
+▄▀▀░░░░░░░█▄░░░░░░▄▀░░░░░░█▄░░
+█░░░░░░░░░░░░░░░░░░░░░░░░░░▀█▄
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+█▄░░█ █▀▀█ ▀▀█▀▀░░█▀▀█ █▀▀█ █▀▀▄
+█░█░█ █░░█ ░░█░░░░█▀▀▄ █▄▄█ █░░█
+█░░▀█ █▄▄█ ░░█░░░░█▄▄█ █░░█ █▄▄▀
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+      abort
+    end
   end
-end
 
-puts "CONFUCIUS SAY GAME OVER STUPID!"
+  puts "CONFUCIUS SAY GAME OVER STUPID!"
+  puts "░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
+  ░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░
+  ░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░
+  ░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░
+  ░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░
+  █░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█
+  █░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
+  ░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░
+  ░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░
+  ░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░
+  ░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░
+  ░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░
+  ░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░
+  ░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░
+  ░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░"
+  puts "==== PRESS ENTER TO CONTINUE! ===="
+  gets
+end
     
